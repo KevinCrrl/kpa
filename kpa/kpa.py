937817ca252947ca774e8b4204d33af762853075
KevinCrrl <y62257136@gmail.com>
2025-11-20 14:25:10 -0500

    Limpiando menú de KPA para que sea más organizado

--- kpa/kpa.py
+++ kpa/kpa.py
@@ -69,15 +69,31 @@ def main():
     try:
         for arg, funcion in args.items():
             if sys.argv[1] == "-h":
-                print("""Argumentos válidos en KPA Versión 1.7.0-beta:
--I paquete para instalar
--A paquete para actualizar un paquete instalado por kpa(o "-A todo" para actualización completa de todo lo instalado con kpa)
--D paquete para desinstalar. -D solo desinstala paquetes instalados por este AUR helper, no desinstala paquetes de otras fuentes como otro AUR helper o Pacman.
--C paquete para consultar sobre un paquete (abre un navegador con la página del paquete en el AUR)
--R paquete para reinstalar un paquete instalado por kpa
--L debug/huerfanos, -L debug elimina todo paquetes que tenga -debug en su nombre, -L huerfanos elimina todo paquete que no sea necesitado por otro y no haya sido instalado por el usuario.
+                print("""Argumentos válidos en KPA versión 1.7.0-beta:
 
-Recuerde crear el archivo kpa.json para configurar kpa correctamente, vea en https://KevinCrrl.github.io/KevinCrrl/documentacion/kpa.html un ejemplo de como debería ser el archivo.""")
+    Instalar paquetes:
+        -I [PAQUETES]
+
+    Actualizar paquetes:
+        -A [PAQUETES]
+    Actualizar TODOS los paquetes instalados con KPA:
+        -A todo
+
+    Reinstalar paquetes:
+        -R [PAQUETES]
+
+    Limpiar paquetes huérfanos y/o paquetes que sean de tipo '-debug':
+        -L huerfanos
+        -L debug
+
+    Limpiar tanto huérfanos como debug:
+        -L huerfanos debug
+
+    Ver esta ayuda: -h
+
+Recuerde crear el archivo kpa.json para configurar kpa correctamente,
+vea en https://KevinCrrl.github.io/KevinCrrl/documentacion/kpa.html un
+ejemplo de como debería ser el archivo.""")
                 break
             if sys.argv[1] == arg:
                 for paquete in sys.argv[2:]:
